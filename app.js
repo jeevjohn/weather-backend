7d44bbaa145f30b5cea222c72ec493fadfbd3516
jeevjohn <97367058+jeevjohn@users.noreply.github.com>
2022-02-05 23:45:20 -0800

    seond commit

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 const key = "eyJ1c2VybmFtZSI6IkplZXZhbiIsInBhc3N3b3JkICI6ImplZXYiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.lG8nhw_gAXyso4H0avrX1h9uXyvot73gwTXVp8RFczA"
-const expiry = new Date()
-expiry.setDate(expiry.getDate() + 7);
+
 
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
